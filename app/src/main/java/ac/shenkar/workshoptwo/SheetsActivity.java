b34a7ce640e5a7577aa32de79ee31c8ac83c2e6d
amir <amir@mindtheapps.com>
2017-06-04 17:28:24 +0300

    added "add" functionality example to Sheets

--- app/src/main/java/ac/shenkar/workshoptwo/SheetsActivity.java
+++ app/src/main/java/ac/shenkar/workshoptwo/SheetsActivity.java
@@ -75,20 +75,6 @@ public class SheetsActivity extends Activity
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.sheet_activity_layout);
-
-//        LinearLayout activityLayout = new LinearLayout(this);
-//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.MATCH_PARENT,
-//                LinearLayout.LayoutParams.MATCH_PARENT);
-//        activityLayout.setLayoutParams(lp);
-//        activityLayout.setOrientation(LinearLayout.VERTICAL);
-//        activityLayout.setPadding(16, 16, 16, 16);
-//
-//        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT);
-//
-//        binding.buttonGet = new Button(this);
         binding.buttonGet.setText(BUTTON_TEXT);
         binding.buttonGet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,21 +85,13 @@ public class SheetsActivity extends Activity
                 binding.buttonGet.setEnabled(true);
             }
         });
-//        activityLayout.addView(binding.buttonGet);
 
-//        binding.theList = new TextView(this);
-//        binding.theList.setLayoutParams(tlp);
-//        binding.theList.setPadding(16, 16, 16, 16);
-//        binding.theList.setVerticalScrollBarEnabled(true);
         binding.theList.setMovementMethod(new ScrollingMovementMethod());
         binding.theList.setText(
                 "Click the \'" + BUTTON_TEXT + "\' button to test the API.");
-//        activityLayout.addView(binding.theList);
 
         mProgress = new ProgressDialog(this);
         mProgress.setMessage("Calling Google Sheets API ...");
-
-//        setContentView(activityLayout);
 
         // Initialize credentials and service object.
         mCredential = GoogleAccountCredential.usingOAuth2(
